e68bb29645f2b95612a9537531ae89721d23e1d0
Frank Eller <fe@frankeller.de>
2025-05-24 08:07:11 +0200

    initial commit

---
+++ Fe.Extensions/EnumerableExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fe.Extensions
+{
+	/// ---------------------------------------------------------------------------------------------------------------------------
+	/// <summary>
+	/// Provides extension methods for working with <see cref="IEnumerable{T}"/> sequences.
+	/// </summary>
+	/// <remarks>
+	/// This static class contains utility methods that extend the functionality of enumerable sequences,
+	/// such as filtering out null elements from a sequence of reference types.
+	/// </remarks>
+	/// ---------------------------------------------------------------------------------------------------------------------------
+	public static class EnumerableExtensions
+	{
+		/// ---------------------------------------------------------------------------------------------------------------------------
+		/// <summary>
+		/// Filters a sequence of reference type elements, returning only the non-null elements.
+		/// </summary>
+		/// <typeparam name="T">The reference type of the elements in the source sequence.</typeparam>
+		/// <param name="source">The sequence to filter.</param>
+		/// <returns>An <see cref="IEnumerable{T}"/> that contains only the non-null elements from the input sequence.</returns>
+		/// <exception cref="ArgumentNullException">Thrown if <paramref name="source"/> is null.</exception>
+		/// ---------------------------------------------------------------------------------------------------------------------------
+		public static IEnumerable<T> WhereNotNull<T>( this IEnumerable<T?> source ) where T : class
+		{
+			var result = source is null
+				? throw new ArgumentNullException( nameof( source ) )
+				: source.Where( item => item is not null ).Select( item => item! );
+
+			return result;
+		}
+	}
+}
+
---
+++ Fe.Extensions/StringExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fe.Extensions
+{
+	/// ---------------------------------------------------------------------------------------------------------------------------
+	/// <summary>
+	/// Provides extension methods for <see cref="string"/> to simplify common string checks.
+	/// </summary>
+	/// ---------------------------------------------------------------------------------------------------------------------------
+	public static class StringExtensions
+	{
+		/// ---------------------------------------------------------------------------------------------------------------------------
+		/// <summary>
+		/// Determines whether the specified string is not <c>null</c>, empty, or consists only of white-space characters.
+		/// </summary>
+		/// <param name="item">The string to check.</param>
+		/// <returns>
+		/// <c>true</c> if the string has a value (is not <c>null</c>, empty, or whitespace); otherwise, <c>false</c>.
+		/// </returns>
+		/// ---------------------------------------------------------------------------------------------------------------------------
+		public static bool HasValue( [NotNullWhen( true )] this string? item )
+		{
+			return !String.IsNullOrWhiteSpace( item );
+		}
+
+		/// ---------------------------------------------------------------------------------------------------------------------------
+		/// <summary>
+		/// Determines whether the specified string is <c>null</c>, empty, or consists only of white-space characters.
+		/// </summary>
+		/// <param name="item">The string to check.</param>
+		/// <returns>
+		/// <c>true</c> if the string is <c>null</c>, empty, or whitespace; otherwise, <c>false</c>.
+		/// </returns>
+		/// ---------------------------------------------------------------------------------------------------------------------------
+		public static bool IsEmpty( [NotNullWhen( false )] this string? item ) => !item.HasValue();
+
+	}
+}
