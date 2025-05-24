4640c78fe9584dd84ebab47c7ac1542b06e78daf
Frank Eller <fe@frankeller.de>
2025-05-24 08:32:59 +0200

    Added conversions to numeric types

--- Fe.Extensions/StringExtensions.cs
+++ Fe.Extensions/StringExtensions.cs
@@ -14,6 +14,8 @@ namespace Fe.Extensions
 	/// ---------------------------------------------------------------------------------------------------------------------------
 	public static class StringExtensions
 	{
+		#region Checks for Empty
+
 		/// ---------------------------------------------------------------------------------------------------------------------------
 		/// <summary>
 		/// Determines whether the specified string is not <c>null</c>, empty, or consists only of white-space characters.
@@ -38,6 +40,130 @@ namespace Fe.Extensions
 		/// </returns>
 		/// ---------------------------------------------------------------------------------------------------------------------------
 		public static bool IsEmpty( [NotNullWhen( false )] this string? item ) => !item.HasValue();
+
+		#endregion
+
+		#region Conversions
+
+		public static int ToInt32( this string? value, int defaultValue = 0 )
+		{
+			return Int32.TryParse( value, out int result ) ? result : defaultValue;
+		}
+
+		public static int? ToNullableInt32( this string? value, int? defaultValue = null )
+		{
+			return Int32.TryParse( value, out int result ) ? result : defaultValue;
+		}
+
+		public static long ToInt64( this string? value, long defaultValue = 0 )
+		{
+			return Int64.TryParse( value, out long result ) ? result : defaultValue;
+		}
+
+		public static long? ToNullableInt64( this string? value, long? defaultValue = null )
+		{
+			return Int64.TryParse( value, out long result ) ? result : defaultValue;
+		}
+
+		public static double ToDouble( this string? value, double defaultValue = 0 )
+		{
+			return Double.TryParse( value, out double result ) ? result : defaultValue;
+		}
+
+		public static double? ToNullableDouble( this string? value, double? defaultValue = null )
+		{
+			return Double.TryParse( value, out double result ) ? result : defaultValue;
+		}
+
+		public static byte ToByte(this string? value, byte defaultValue = 0)
+		{
+			return Byte.TryParse( value, out byte result ) ? result : defaultValue;
+		}
+
+		public static byte? ToNullableByte( this string? value, byte? defaultValue = null )
+		{
+			return Byte.TryParse( value, out byte result ) ? result : defaultValue;
+		}
+
+		public static Guid ToGuid( this string? value, Guid defaultValue )
+		{
+			return Guid.TryParse( value, out Guid result ) ? result : defaultValue;
+		}
+
+		public static Guid? ToNullableGuid( this string? value )
+		{
+			return Guid.TryParse( value, out Guid result ) ? result : null;
+		}
+		public static float ToSingle( this string? value, float defaultValue = 0 )
+		{
+			return Single.TryParse( value, out float result ) ? result : defaultValue;
+		}
+
+		public static float? ToNullableSingle( this string? value, float? defaultValue = null )
+		{
+			return Single.TryParse( value, out float result ) ? result : defaultValue;
+		}
+
+		public static decimal ToDecimal( this string? value, decimal defaultValue = 0 )
+		{
+			return Decimal.TryParse( value, out decimal result ) ? result : defaultValue;
+		}
+
+		public static decimal? ToNullableDecimal( this string? value, decimal? defaultValue = null )
+		{
+			return Decimal.TryParse( value, out decimal result ) ? result : defaultValue;
+		}
+
+		public static short ToInt16( this string? value, short defaultValue = 0 )
+		{
+			return Int16.TryParse( value, out short result ) ? result : defaultValue;
+		}
+
+		public static short? ToNullableInt16( this string? value, short? defaultValue = null )
+		{
+			return Int16.TryParse( value, out short result ) ? result : defaultValue;
+		}
+
+		public static ushort ToUInt16( this string? value, ushort defaultValue = 0 )
+		{
+			return UInt16.TryParse( value, out ushort result ) ? result : defaultValue;
+		}
+
+		public static ushort? ToNullableUInt16( this string? value, ushort? defaultValue = null )
+		{
+			return UInt16.TryParse( value, out ushort result ) ? result : defaultValue;
+		}
+
+		public static uint ToUInt32( this string? value, uint defaultValue = 0 )
+		{
+			return UInt32.TryParse( value, out uint result ) ? result : defaultValue;
+		}
+
+		public static uint? ToNullableUInt32( this string? value, uint? defaultValue = null )
+		{
+			return UInt32.TryParse( value, out uint result ) ? result : defaultValue;
+		}
+
+		public static ulong ToUInt64( this string? value, ulong defaultValue = 0 )
+		{
+			return UInt64.TryParse( value, out ulong result ) ? result : defaultValue;
+		}
+
+		public static ulong? ToNullableUInt64( this string? value, ulong? defaultValue = null )
+		{
+			return UInt64.TryParse( value, out ulong result ) ? result : defaultValue;
+		}
+
+		public static sbyte ToSByte( this string? value, sbyte defaultValue = 0 )
+		{
+			return SByte.TryParse( value, out sbyte result ) ? result : defaultValue;
+		}
+
+		public static sbyte? ToNullableSByte( this string? value, sbyte? defaultValue = null )
+		{
+			return SByte.TryParse( value, out sbyte result ) ? result : defaultValue;
+		}
+		#endregion
 
 	}
 }
